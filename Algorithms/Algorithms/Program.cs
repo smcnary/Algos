503cc5ce1a6aa994754e2d1fe75a4a4349ffd316
Sean McNary <sean.mcnary@aaon.com>
2019-10-11 11:13:01 -0500

    Adds the bubble sort.

---
+++ Algorithms/Algorithms/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BubbleSort();
+		}
+
+		private static void BubbleSort()
+		{
+			int[] a = { 3, 0, 2, 5, -1, 4, 1 }; 
+			int t; 
+			Console.WriteLine("Original array :");
+			foreach (int aa in a)                       
+				Console.Write(aa + " ");                     
+			for (int passLength = a.Length -2; passLength >= 0; passLength--)
+			{
+				for (int i = 0; i <= passLength; i++)
+				{
+					if (a[i] > a[i + 1])
+					{
+						t = a[i + 1];
+						a[i + 1] = a[i];
+						a[i] = t;
+					}
+				} 
+			}
+			Console.WriteLine("\n"+"Sorted array :");
+			foreach (int aa in a)                       
+				Console.Write(aa + " ");
+			Console.Write("\n"); 
+		}
+	}
+}
