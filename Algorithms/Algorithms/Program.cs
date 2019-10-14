dfd736269342fdcf548fda83f62480049b612caa
Sean McNary <sean.mcnary@aaon.com>
2019-10-14 15:02:07 -0500

    Adds the next bubble sort.

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -1,24 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace Algorithms
 {
 	internal class Program
 	{
+		static readonly object _locker = new object();
 		private static void Main(string[] args)
 		{
 			int[] a = { 3, 0, 2, 5, -1, 4, 1 };
-
-
+			
 			Algos algos = new Algos();
-
 			//Fastest to slowest
-			algos.RadixSort();
+			lock (_locker)
+			{
+				algos.RadixSort();
+			}
+			lock (_locker)
+			{
+				algos.IntArraySelectionSort(a);
+			}
 
-			algos.IntArraySelectionSort(a);
+			lock (_locker)
+			{
+				algos.IntArrayBubbleSort(a);
+			}
 
-			algos.IntArrayBubbleSort(a);
+			lock (_locker)
+			{
+				algos.BubbleSort(a);
+			}
 
 
 		}
