296ae3240266934fae30e5cf97848d423853719e
Sean McNary <sean.mcnary@aaon.com>
2020-01-09 15:01:51 -0600

    adds timing method

--- Algorithms/Algorithms/Collection.cs
+++ Algorithms/Algorithms/Collection.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,7 +27,61 @@ namespace Algorithms
 		}
 
 		public new int Count() => InnerList.Count;
-	}
 
-	public class T { }
+
+		class Chapter1
+		{
+			static void Main() {
+				int[] nums = new int[1000000];
+				BuildArray(nums);
+				TimeSpan startTime;
+				TimeSpan duration;
+				startTime =
+						Process.GetCurrentProcess().Threads[0].
+						        UserProcessorTime;
+				DisplayNums(nums);
+				duration =
+						Process.GetCurrentProcess().Threads[0].
+						        UserProcessorTime.
+						        Subtract(startTime);
+				Console.WriteLine("Time:"+ duration.TotalSeconds);
+
+				Console.ReadLine();
+			}
+			static void BuildArray(int[] arr) {
+				for(int i = 0; i <= 999999; i++)
+					arr[i] = i;
+			}
+			static void DisplayNums(int[] arr) {
+				for(int i = 0; i <= arr.GetUpperBound(0); i++)
+					Console.Write(arr[i] + " ");
+			}
+
+			static void Swap<T>(ref T val1, ref T val2)
+			{
+				T temp;
+				temp = val1;
+				val1 = val2;
+				val2 = temp;
+				
+			}
+
+			
+		}
+
+		public class Node<T>
+		{
+			private T data;
+			private Node<T> link;
+
+			public Node(T data, Node<T> link)
+			{
+				this.data = data;
+				this.link = link;
+			}
+		}
+
+
+	}
+	
 }
--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -12,38 +12,38 @@ namespace Algorithms
 		static readonly object _locker = new object();
 		private static IList nums;
 
-		private static void Main(string[] args)
-		{
-			//int[] a = { 3, 0, 2, 5, -1, 4, 1 };
+		//private static void Main(string[] args)
+		//{
+		//	//int[] a = { 3, 0, 2, 5, -1, 4, 1 };
 			
-			//SortingAlgorithms sortingAlgorithms = new SortingAlgorithms();
-			////Fastest to slowest
-			//lock (_locker)
-			//{
-			//	sortingAlgorithms.RadixSort();
+		//	//SortingAlgorithms sortingAlgorithms = new SortingAlgorithms();
+		//	////Fastest to slowest
+		//	//lock (_locker)
+		//	//{
+		//	//	sortingAlgorithms.RadixSort();
 
-			//}
-			//lock (_locker)
-			//{
-			//	sortingAlgorithms.IntArraySelectionSort(a);
-			//}
+		//	//}
+		//	//lock (_locker)
+		//	//{
+		//	//	sortingAlgorithms.IntArraySelectionSort(a);
+		//	//}
 
-			//lock (_locker)
-			//{
-			//	sortingAlgorithms.IntArrayBubbleSort(a);
-			//}
+		//	//lock (_locker)
+		//	//{
+		//	//	sortingAlgorithms.IntArrayBubbleSort(a);
+		//	//}
 
-			//lock (_locker)
-			//{
-			//	sortingAlgorithms.BubbleSort(a);
-			//}
+		//	//lock (_locker)
+		//	//{
+		//	//	sortingAlgorithms.BubbleSort(a);
+		//	//}
 
-			SortingAlgorithms sortingAlgorithms = new SortingAlgorithms();
-			BinaryTreeTraversals binaryTreeTraversals = new BinaryTreeTraversals();
+		//	SortingAlgorithms sortingAlgorithms = new SortingAlgorithms();
+		//	BinaryTreeTraversals binaryTreeTraversals = new BinaryTreeTraversals();
 
 			
 
-		}
+		//}
 
 		
 
