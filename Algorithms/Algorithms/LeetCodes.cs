7f6b1613e0c09ccff3b6eb28e08ac8befc459746
Sean McNary <sean.mcnary@aaon.com>
2020-01-31 07:14:23 -0600

    adds another leedcode algo

--- Algorithms/Algorithms/LeetCodes.cs
+++ Algorithms/Algorithms/LeetCodes.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -42,6 +43,27 @@ namespace Algorithms
 			//	result += v
 			//}
 			return result;
+		}
+
+		public int NumJewelsInStones(string J, string S)
+		{
+			//initialize hashet for J.
+			var jset = new HashSet<char>();
+			foreach (var j in J.ToCharArray())
+			{
+				jset.Add(j);
+			}
+
+			int ans = 0;
+			foreach (var s in S.ToCharArray())
+			{
+				if (jset.Contains(s))
+					ans++;
+			}
+			
+			//return (from j in J from s in S where j == s select s).Count();
+
+			return ans;
 		}
 	}
 }
